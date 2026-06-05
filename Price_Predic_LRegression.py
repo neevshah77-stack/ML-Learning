7041482f7a2d42212f8c95a56d60c3df5271e65b
Neev Shah <neevshah77@gmail.com>
2026-06-05 22:31:10 +0530

    Update Price_Predic_LRegression.py

--- Price_Predic_LRegression.py
+++ Price_Predic_LRegression.py
@@ -1,10 +1,13 @@
+#Importing numpy, matplotlib.pyplot
 import numpy as np
 import matplotlib.pyplot as plt
-
+#Training dataset
 x_train = np.array([1.0,2.0])
 y_train = np.array([300.0,500.0])
+#Let
 w = 200
 b = 100
+#Define a function to get the target variable(y)
 def compute_model_output(x,w,b):
   m = x.shape[0]
   f_wb = np.zeros(m)
@@ -12,11 +15,11 @@ def compute_model_output(x,w,b):
     f_wb[i] = w * x[i] + b
 
   return f_wb
-
+#Store y in tmp_f_wb
 tmp_f_wb = compute_model_output(x_train,w,b,)
 
-plt.plot(x_train,tmp_f_wb,c='b',label='Our prediction')
-plt.scatter(x_train,y_train,marker='o',c='r',label='Acutal Values')
+plt.plot(x_train,tmp_f_wb,c='b',label='Our prediction')#Plot the fitting line
+plt.scatter(x_train,y_train,marker='o',c='r',label='Acutal Values')#Plot data points
 plt.title("Housing Prices")
 plt.ylabel('Price')
 plt.xlabel('Size')
